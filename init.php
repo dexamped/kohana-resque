ad24b4ec276d8657295c6bbe0dfa1f6a476cf153
Chris <dexamped@gmail.com>
2015-05-21 14:45:46 +1000

    Removed manual autoloading

--- init.php
+++ init.php
@@ -1,21 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-// Simple autoloader for Resque
-class Resque_Autoload {
-
-	public static function autoload($class)
-	{
-		return Kohana::auto_load($class, 'vendor/php-resque/lib');
-	}
-}
-
-// Register the autoloader
-spl_autoload_register(array('Resque_Autoload', 'autoload'));
-
-// Autoload vendor dependencies
-require_once Kohana::find_file('vendor/php-resque', 'vendor/autoload');
-
-
 // Load Resque config as environment variables
 foreach (Kohana::$config->load('resque')->as_array() as $var => $param)
 {
